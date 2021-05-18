14bac35b50a7305233ae336e3bb2886ec1693766
Dustin Clifford <dc.christianson@gmail.com>
2021-05-17 18:53:41 -0600

    2nd commit

--- main.js
+++ main.js
@@ -7,5 +7,19 @@ let gameWindow = document.getElementById("gameWindow");
 for(i=0; i < 9; i++){
     let boardCell = document.createElement("div");
     boardCell.classList.add("boardCell");
+    boardCell.setAttribute("id", i);
     gameWindow.appendChild(boardCell);
 }
+
+//store array of plays in memory
+
+let boardCellPlays = [];
+
+//write player move to board cell
+
+for(i=0; i < 9; i++){
+    let thisBoardCell = document.getElementById(i);
+    thisBoardCell.addEventListener("click", console.log("clicked cell"));
+}
+
+
